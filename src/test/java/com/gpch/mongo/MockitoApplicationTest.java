613626dcab09543db71c493a48f9ee6ea67edc8c
vasanthakumarmx94 <vasanthakumarmx94@gmail.com>
2022-02-18 16:26:52 +0530

    Initial commit

---
+++ src/test/java/com/gpch/mongo/MockitoApplicationTest.java
@@ -0,0 +1,15 @@
+package com.gpch.mongo;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MockitoApplicationTest {
+
+//	@Test
+//    public void main() {
+//		MockitoApplicationTest.main(new String[] {});
+//    }
+	
+	
+}
